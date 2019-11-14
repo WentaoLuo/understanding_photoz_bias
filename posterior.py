53239aefb456744b1258dfe5add587faf67e8cc8
Wentao Luo <wtluo@MasahironoMacBook-Pro.local>
2019-11-14 10:48:43 +0800

    toy pofz

---
+++ posterior.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+pi = np.pi
+
+def gaussian(z0,sig):
+  zz    = np.linspace(0.1,1.1,500)
+  gauss = (1.0/np.sqrt(2.0*pi*(sig)))*np.exp(-0.5*(zz-z0)*(zz-z0)/sig/sig)
+  """
+  print (gauss/gauss.sum()).sum()
+  plt.plot(zz,gauss,'k-')
+  plt.show()
+  """
+  return gauss/gauss.sum()
+
+def twogaussian(z0,dz,sig,sig1,ratio):
+  zz     = np.linspace(0.1,1.1,500)
+  amp    = ratio
+  gauss1 = amp*(1.0/np.sqrt(2.0*pi*(sig)))*np.exp(-0.5*(zz-z0)*(zz-z0)/sig/sig)
+  gauss2 = (1.0/np.sqrt(2.0*pi*(sig1)))*np.exp(-0.5*(zz-z0-dz)*(zz-z0-dz)/sig1/sig1)
+  gauss  = (gauss1+gauss2)/(gauss1+gauss2).sum()
+  #gauss  = (gauss1+gauss2)
+  """
+  plt.plot(zz,gauss,'k-',label='Total')
+  plt.plot(zz,gauss1,'r-',label='Gaussian 1')
+  plt.plot(zz,gauss2,'b-',label='Gaussian 2')
+  plt.xlabel('z')
+  plt.ylabel('P(z)')
+  plt.legend()
+  plt.savefig('mock_PofZ_1.eps')
+  plt.show()
+  """
+  return gauss
+
+"""
+def main():
+
+  #gaussian(0.4,0.05)
+  twogaussian(0.4,0.3,0.05,0.1,10.0)
+
+if __name__=="__main__":
+  main()
+"""
